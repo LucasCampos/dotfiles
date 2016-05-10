fa95aeeb2ce00be66d41b1ffd2e3338e8d03a783
Lucas Campos <lqcc@df.ufpe.br>
2016-05-10 11:46:12 -0300

    Added part to copy dotfiles

--- prepare.sh
+++ prepare.sh
@@ -6,3 +6,9 @@ bash installs/install_science.sh
 bash installs/install_tools.sh
 bash installs/install_vim.sh
 bash installs/install_zsh.sh
+
+cp -r .config ~
+cp -r .gitconfig ~
+cp -r .startup.py ~
+cp -r .ycm_extra_conf.py ~
+cp -r .zshrc ~
