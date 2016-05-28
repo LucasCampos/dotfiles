e0ad6bd81c119a9deaa752a73b6a21984688a60a
Lucas Campos <lqcc@df.ufpe.br>
2016-05-28 07:57:02 -0300

    Now installing clipboard manager xsel together with vim

--- installs/install_vim.sh
+++ installs/install_vim.sh
@@ -7,7 +7,7 @@ sudo apt-get install python-dev python-pip python3-dev python3-pip -y
 
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get install neovim -y
+sudo apt-get install neovim xsel -y
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
